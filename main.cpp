8d1828815b3fc6393b45434588b1fe31c3a04063
qagr49 <qagr49@mail.umkc.edu>
2017-02-27 15:16:48 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-int main(){}
+
+#include <iostream>
+#include "userInterface.h"
+#include "list_iterator.h"
+
+using namespace std;
+
+
+int main() {
+
+	userInterface interface;
+	interface.run();
+}
+
